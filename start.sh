868976a85a5fd931714681efed921037f00e2a81
ashishsantikari <hi@ashishsantikari.info>
2021-09-29 13:56:38 +0200

    replaces rpiPlay with UxPlay

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/vc/lib
 
 DEVICE_NAME="${AIRPIPLAY_NAME:="Air-Pi-Play@$HOSTNAME"}"
 AUDIO_OUT="${AIRPIPLAY_AUDIO_OUT:="hdmi"}"
-BACKGROUND="${AIRPIPLAY_BACKGROUND:="auto"}"
 case $AIRPIPLAY_LATENCY_MODE in
   1) LATENCY="-l";;
   0) LATENCY="";;
@@ -16,4 +15,4 @@ echo "Air-Pi-Play v$(cat VERSION) powered by RpiPlay"
 
 echo "Device is discoverable as ${DEVICE_NAME}"
 
-rpiplay -n $DEVICE_NAME -a $AUDIO_OUT -b $BACKGROUND $LATENCY $ROTATION $FLIP
+uxplay -n $DEVICE_NAME -a $AUDIO_OUT -b $LATENCY $ROTATION $FLIP
